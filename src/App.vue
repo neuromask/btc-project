4bc760ed114d102f3295d6eafe16696b68adace3
neuromask <neuromask@gmail.com>
2019-01-29 01:08:27 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <btc msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import btc from './components/btc.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    btc
   }
 }
 </script>
--- src/components/btc.vue
+++ src/components/btc.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'btc',
   props: {
     msg: String
   }
