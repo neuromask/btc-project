1fa0b7dedfd033546c623e42562ae82d9d81a62a
neuromask <neuromask@gmail.com>
2019-01-31 01:48:09 +0200

    upd 5.1

---
+++ src/components/footerBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <!-- As a link -->
+        <b-navbar type="dark" variant="dark" toggleable class="dark-footer">
+            <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
+            <b-collapse is-nav id="nav_dropdown_collapse">
+                <b-navbar-nav>
+                    <b-nav-item href="#">Home</b-nav-item>
+                    <b-nav-item href="#">Link</b-nav-item>
+                    <b-nav-item href="#">Home</b-nav-item>
+                    <b-nav-item href="#">Link</b-nav-item>
+                    <b-nav-item href="#">Home</b-nav-item>
+                    <b-nav-item href="#">Link</b-nav-item>
+                </b-navbar-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-navbar-brand href="#" class="mr-0">
+                        <i class="fab fa-facebook-square nav-link"></i>
+                    </b-navbar-brand>
+                    <b-navbar-brand href="#" class="mr-0">
+                        <i class="fab fa-vk nav-link"></i>
+                    </b-navbar-brand>
+                    <b-navbar-brand href="#" class="mr-0">
+                        <i class="fab fa-youtube-square nav-link"></i>
+                    </b-navbar-brand>
+                    <b-navbar-brand href="#" class="mr-0">
+                        <i class="fab fa-instagram nav-link"></i>
+                    </b-navbar-brand>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "footerBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
